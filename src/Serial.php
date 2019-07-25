678e53ccdf660eefc7f090a3e94cf64c608ef0a5
Bennet Gallein <bg@esyoil.com>
2019-07-25 11:23:47 +0200

    init

---
+++ src/Serial.php
@@ -0,0 +1,27 @@
+<?php
+namespace Serial;
+
+class Serial {
+
+    public function __construct() {
+        
+    }
+
+    public function setup() {
+
+    }
+
+    public function open() {
+
+    }
+
+    public function read() {
+
+    }
+
+    public function write() {
+
+    }
+
+
+}
